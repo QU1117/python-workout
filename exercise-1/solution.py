92fb5540a1b916faebd14a9bcaa7ae75201afbc4
QU1117 <volgirre@gmail.com>
2023-11-19 17:56:56 +0300

    exercise 1 done

--- exercise-1/solution.py
+++ exercise-1/solution.py
@@ -4,17 +4,25 @@ def guessing_game():
     number = randint(0, 100)
 
     while True:
-        guess = input("Guess what number has been chosen: ")
+        guess = input("Guess what number (integer) has been chosen: ")
 
         try:
-            isinstance(int(guess), int)
-            break
-        except ValueError:
-            print('''You have typed something which is not an 
-                  integer, please try again''')
-            continue
+            if (isinstance(int(guess), int)):
+                guess = int(guess)
 
-    print(guess)
+            if guess > number:
+                print('Too high, try again\n')
+                continue
+            elif guess < number:
+                print('Too low, try again\n')
+                continue
+            else:
+                print(f'Just right, the number is {number}\n')
+                break
+
+        except ValueError:
+            print('''You have typed something which is not an integer, please try again\n''')
+            continue
 
 def main():
     guessing_game()
